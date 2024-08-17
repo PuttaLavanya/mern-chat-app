8b91f39d8a6103bc902a60f98c303fc74d3141e7
plavanya <plavanya@enphaseenergy.com>
2024-08-17 16:19:30 +0530

    deployment

--- backend/server.js
+++ backend/server.js
@@ -1,3 +1,4 @@
+import path from "path";
 import express from "express" ;
 import dotenv from "dotenv";
 import cookieParser from "cookie-parser";
@@ -11,8 +12,12 @@ import connectToMongoDB from "./db/connectToMongoDB.js";
 import { app, server } from "./socket/socket.js";
 
 
+const __dirname = path.resolve();
+
 dotenv.config();
 const port = 5000 || process.env.PORT;
+
+
 
 
 app.use(express.json());
@@ -21,6 +26,13 @@ app.use((cookieParser()));
 app.use("/api/auth",authRoutes);
 app.use("/api/messages",messageRoutes);
 app.use("/api/users",userRoutes);
+
+
+app.use(express.static(path.join(__dirname, "/frontend/dist")));
+
+app.get("*", (req, res) => {
+	res.sendFile(path.join(__dirname, "frontend", "dist", "index.html"));
+});
 
 // app.get("/",(req,res)=>{
 //     res.send("Om Namashivaya!!!");
